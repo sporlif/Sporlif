633a11f4871ae192be5394f3f1c397b2226c250c
Mauricio Pinzon <sporlif@gmail.com>
2018-09-23 19:05:23 -0500

    Creción de proyecto Sporlif con Kotlin

---
+++ app/src/main/java/com/sporlif/sporlif/user/View/ActLogin.kt
@@ -0,0 +1,13 @@
+package com.sporlif.sporlif.user.View
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sporlif.sporlif.R
+
+class ActLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_login)
+    }
+}
